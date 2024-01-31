eb31a65d9393f3717017895dc1d05e6cf69cad0d
Alex S <alexs1010@gmail.com>
2024-01-31 10:52:15 -0600

    add progress circular and  widget folder and page view to add scroll view close #6

--- lib/presentation/screens/discover/discover_screen.dart
+++ lib/presentation/screens/discover/discover_screen.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tiktok_practice/presentation/providers/discover_provider.dart';
+import 'package:flutter_tiktok_practice/presentation/widgets/shared/video_scrollable_view.dart';
+import 'package:provider/provider.dart';
 
 class DiscoverScreen extends StatelessWidget{
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context){
-    return const Scaffold(
-      body: Center(child: Text('Discover screen'),)
+
+    final discoverProvider = context.watch<DiscoverProvider>();
+
+    //other way
+    //final otroProvider = Provider.of<DiscoverProvider>(context, listen: false);
+
+    return Scaffold(
+      body: discoverProvider.initialLoading ? const Center(child: CircularProgressIndicator(),) : VideoScrollableView(videos: discoverProvider.videos)
     );
   }
 }
